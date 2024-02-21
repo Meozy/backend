59d4a416273955e7c2dd78e19bd8b65749089537
caio-couto <cavalcantecaio.couto@gmail.com>
2024-02-21 09:59:56 -0300

    refactor: improvements to the Email class

--- src/modules/user/domain/models/Email.ts
+++ src/modules/user/domain/models/Email.ts
@@ -7,50 +7,38 @@ class Email
   private constructor (email: string)
   {
     this.email = email;
+    Object.freeze(this);
   }
 
   static create (email: string): Result<Email>
   {
-    if (!this.validate(email))
-    {
-      return Result.Err("Invalid Email");
-    }
+    if (!this.validate(email)) return Result.Err("Invalid Email");
 
-    return Result.Ok(new Email("email"));
+    return Result.Ok(new Email(email));
   }
 
-  private static validate(email: string): boolean
+  static validate(email: string): boolean
   {
     const emailExpression = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
 
-    if (!email)
-    {
-      return false;
-    }
+    if (!email) return false;
 
-    if (email.length > 256)
-    {
-      return false;
-    }
-
-    if (!emailExpression.test(email))
-    {
-      return false;
-    }
+    if (email.length > 256) return false;
+    
+    if (!emailExpression.test(email)) return false;
 
     const [account, address] = email.split("@");
 
-    if (account.length > 64 || account.length === 0)
-    {
-      return false;
-    }
+    if (account.length > 64 || account.length === 0) return false;
 
-    if (address.length > 255 || address.length === 0)
-    {
-      return false;
-    }
-
+    if (address.length > 255 || address.length === 0) return false;
+  
     return true;
+  }
+
+  toString(): string 
+  {
+    return this.email;
   }
 
   getValue(): string
